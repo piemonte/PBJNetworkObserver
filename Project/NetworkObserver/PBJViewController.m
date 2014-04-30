38df2da56267c715ef58343e484ff8b101a4eab5
Patrick Piemonte <patrick@diy.org>
2014-04-29 17:10:25 -0700

    update comment headers

--- Project/NetworkObserver/PBJViewController.m
+++ Project/NetworkObserver/PBJViewController.m
@@ -1,10 +1,8 @@
 //
 //  PBJViewController.m
-//  NetworkObserver
 //
 //  Created by Patrick Piemonte on 7/22/13.
-//
-//  Copyright (c) 2013-2014 Patrick Piemonte (http://patrickpiemonte.com)
+//  Copyright (c) 2013-present, Patrick Piemonte, http://patrickpiemonte.com
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of
 //  this software and associated documentation files (the "Software"), to deal in
